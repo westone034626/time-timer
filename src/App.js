74467fc10098dd8b85b4e927ad6a9f0f32e15592
seows <seows@laivdata.com>
2022-01-10 15:50:57 +0900

    작동 방식 변경

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [targetMinutes, setTargetMinutes] = useState(0);
   const [currentSeconds, setCurrentSeconds] = useState(0);
   const [isMute, setIsMute] = useState(true);
+  const [isStart, setIsStart] = useState(false);
   const onChange = (e) => {
     const {
       target: { value },
@@ -13,24 +14,31 @@ function App() {
     if (value === '') {
       setTargetMinutes(0);
       setCurrentSeconds(0);
+    } else if (value >= 60) {
+      setTargetMinutes(60);
+      setCurrentSeconds(60 * 60);
+      setIsStart(false);
     } else {
       setTargetMinutes(parseInt(value, 10));
+      setCurrentSeconds(parseInt(value, 10) * 60);
+      setIsStart(false);
     }
   };
 
   useEffect(() => {
-    if (!targetMinutes) return;
+    if (!targetMinutes || !isStart) return;
     let interval = setInterval(() => {
-      setCurrentSeconds((prev) => prev + 1);
+      setCurrentSeconds((prev) => {
+        return !prev ? prev : prev - 1;
+      });
     }, 1000);
     return () => {
-      setCurrentSeconds(0);
       clearInterval(interval);
     };
-  }, [targetMinutes]);
+  }, [targetMinutes, isStart]);
   const playAudio = async () => {
     try {
-      var audio = new Audio('wrist-watch.mp3');
+      var audio = new Audio('/time-timer/wrist-watch.mp3');
       await audio.play();
     } catch (error) {
       throw Error(error);
@@ -38,7 +46,7 @@ function App() {
   };
   useEffect(() => {
     if (!targetMinutes) return;
-    if (currentSeconds === targetMinutes * 60) {
+    if (!currentSeconds) {
       !isMute && playAudio();
     }
   }, [currentSeconds, isMute]);
@@ -48,54 +56,78 @@ function App() {
         textAlign: 'center',
         maxWidth: '500px',
         margin: '0 auto',
+        paddingBottom: '50px',
+        paddingLeft: '50px',
+        paddingRight: '50px',
       }}
     >
       <div
         style={{
           paddingTop: '50px',
-          display: 'flex',
-          flexDirection: 'column',
-          width: 'max-content',
+          paddingBottom: '50px',
+          display: 'grid',
+          gridAutoRows: 'minmax(40px, auto)',
+          gridTemplateColumns: '1fr 1fr 1fr',
+          alignItems: 'center',
           margin: '0 auto',
           gap: '10px',
         }}
       >
-        <div style={{ display: 'flex', alignItems: 'center', gap: 5 }}>
-          목표 시간(분):{' '}
-          <input onChange={onChange} value={targetMinutes}></input>
-          <button
-            onClick={() => {
-              setCurrentSeconds(0);
-              setTargetMinutes(0);
-            }}
-          >
-            리셋
-          </button>
-          <button
-            onClick={() => {
-              setIsMute((prev) => !prev);
-            }}
-          >
-            {isMute ? '알림 사운드 활성화하기' : '알림 음소거하기'}
-          </button>
-        </div>
-        <div style={{ display: 'flex', alignItems: 'center', gap: 5 }}>
-          색 선택:{' '}
-          <input
-            type="color"
-            value={color}
-            onChange={(e) => {
-              setColor(e.target.value);
-            }}
-          />
-        </div>
+        <p
+          style={{
+            wordBreak: 'keep-all',
+            gridRow: '1 / 2',
+            gridColumn: '1 / 2',
+          }}
+        >
+          목표 시간(최대 60분):
+        </p>
+        <input
+          style={{ gridRow: '1 / 2', gridColumn: '2 / 4' }}
+          onChange={onChange}
+          value={targetMinutes}
+        ></input>
+
+        <p style={{ gridRow: '2 / 3', gridColumn: '1 / 2' }}> 색 선택:</p>
+        <input
+          type="color"
+          value={color}
+          onChange={(e) => {
+            setColor(e.target.value);
+          }}
+          style={{ gridRow: '2 / 3', gridColumn: '2 / 4' }}
+        />
+
+        <button
+          style={{ gridRow: '3 / 4', gridColumn: '1 / 2' }}
+          onClick={() => {
+            setIsMute((prev) => !prev);
+          }}
+        >
+          {isMute ? '알림 on' : '알림 off'}
+        </button>
+        <button
+          disabled={!targetMinutes}
+          style={{ gridRow: '3 / 4', gridColumn: '2 / 4' }}
+          onClick={() => {
+            isStart
+              ? (() => {
+                  setCurrentSeconds(0);
+                  setTargetMinutes(0);
+                  setIsStart(false);
+                })()
+              : setIsStart(true);
+          }}
+        >
+          {isStart ? '리셋' : '시작'}
+        </button>
       </div>
-      <h1>{`목표시간: ${targetMinutes}분`}</h1>
-      <h1>{`경과시간: ${
+      {/* <h1>{`목표시간: ${targetMinutes}분`}</h1>
+      <h1>{`남은시간: ${
         Math.floor(currentSeconds / 60)
           ? `${Math.floor(currentSeconds / 60)}분`
           : ''
-      } ${currentSeconds % 60}초`}</h1>
+      } ${currentSeconds % 60}초`}</h1> */}
 
       <div
         style={{
@@ -115,13 +147,11 @@ function App() {
             left: 0,
             right: 0,
             opacity: 0.8,
-            background: `conic-gradient(white ${
-              (currentSeconds / 60 / targetMinutes) * 360 || 0
-            }deg, ${color} ${
-              (currentSeconds / 60 / targetMinutes) * 360 || 0
-            }deg)`,
+            background: `conic-gradient(${color} ${
+              (currentSeconds / 60) * 6
+            }deg, 
+            white ${(currentSeconds / 60) * 6}deg)`,
             borderRadius: '50%',
-            border: '1px solid black',
           }}
         ></div>
       </div>
