990c400c95691d72bfb278a0c2fcde188660c03e
seows <seows@laivdata.com>
2022-01-10 12:49:09 +0900

    implementation complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,115 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState('#ff0000');
+  const [targetMinutes, setTargetMinutes] = useState(0);
+  const [currentSeconds, setCurrentSeconds] = useState(0);
+  const [isMute, setIsMute] = useState(true);
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    if (value === '') {
+      setTargetMinutes(0);
+      setCurrentSeconds(0);
+    } else {
+      setTargetMinutes(parseInt(value, 10));
+    }
+  };
+
+  useEffect(() => {
+    if (!targetMinutes) return;
+    let interval = setInterval(() => {
+      setCurrentSeconds((prev) => prev + 1);
+    }, 1000);
+    return () => {
+      setCurrentSeconds(0);
+      clearInterval(interval);
+    };
+  }, [targetMinutes]);
+  const playAudio = async () => {
+    try {
+      var audio = new Audio('wrist-watch.mp3');
+      await audio.play();
+    } catch (error) {
+      throw Error(error);
+    }
+  };
+  useEffect(() => {
+    if (!targetMinutes) return;
+    if (currentSeconds === targetMinutes * 60) {
+      !isMute && playAudio();
+    }
+  }, [currentSeconds, isMute]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        textAlign: 'center',
+        maxWidth: '500px',
+        margin: '0 auto',
+      }}
+    >
+      <div
+        style={{
+          paddingTop: '50px',
+          display: 'flex',
+          flexDirection: 'column',
+          width: 'max-content',
+          margin: '0 auto',
+          gap: '10px',
+        }}
+      >
+        <div style={{ display: 'flex', alignItems: 'center', gap: 5 }}>
+          목표 시간(분):{' '}
+          <input onChange={onChange} value={targetMinutes}></input>
+          <button
+            onClick={() => {
+              setCurrentSeconds(0);
+              setTargetMinutes(0);
+            }}
+          >
+            리셋
+          </button>
+          <button
+            onClick={() => {
+              setIsMute((prev) => !prev);
+            }}
+          >
+            {isMute ? '알림 사운드 활성화하기' : '알림 음소거하기'}
+          </button>
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center', gap: 5 }}>
+          색 선택:{' '}
+          <input
+            type="color"
+            value={color}
+            onChange={(e) => {
+              setColor(e.target.value);
+            }}
+          />
+        </div>
+      </div>
+      <h1>{`목표시간: ${targetMinutes}분`}</h1>
+      <h1>{`경과시간: ${
+        Math.floor(currentSeconds / 60)
+          ? `${Math.floor(currentSeconds / 60)}분`
+          : ''
+      } ${currentSeconds % 60}초`}</h1>
+
+      <div
+        style={{
+          height: 0,
+          paddingBottom: '100%',
+          background: `conic-gradient(white ${
+            (currentSeconds / 60 / targetMinutes) * 360 || 0
+          }deg, ${color} ${
+            (currentSeconds / 60 / targetMinutes) * 360 || 0
+          }deg)`,
+          borderRadius: '50%',
+          border: '1px solid black',
+        }}
+      ></div>
     </div>
   );
 }
